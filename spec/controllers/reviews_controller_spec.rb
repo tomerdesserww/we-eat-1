a27118d329b04dadfcedd1a1b4c06741186d11b0
Barak Cohen <barak.cohen@wework.com>
2017-06-19 11:40:58 +0300

    add test case to ensure a review for a non-existing restaurant cannot be created

--- spec/controllers/reviews_controller_spec.rb
+++ spec/controllers/reviews_controller_spec.rb
@@ -18,5 +18,12 @@ RSpec.describe ReviewsController, type: :controller do
       expect(json).to include(serialized_review(review).except("id"))
       expect(json[:id]).not_to be_nil
     end
+
+    it 'should fail to create a review for non-existing restaurant' do
+      review = build(:restaurant_review, restaurant_id: -1)
+      post :create, params: parameterize_review(review)
+      expect(response).to have_http_status(:bad_request)
+      expect(Restaurant::Review.count).to eq(0)
+    end
   end
 end
